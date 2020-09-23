4b270064a61ba668ff5e4f26e7a55c9493917573
hagerabosheref <hagersheref747@gmail.com>
2020-09-23 16:51:33 +0200

    create project

---
+++ index.js
@@ -0,0 +1,37 @@
+//variables
+var inputUser=document.querySelector('.input-user');
+//console.log(inputUser);
+var loc=document.querySelector('.location p');
+//console.log(loc);
+var date=document.querySelector('.date p');
+//console.log(date);
+var iconWeather=document.querySelector('.icons-weather');
+//console.log(iconWeather);
+var tempValue=document.querySelector('.temperature-value p');
+//console.log(tempValue);
+var tempDes=document.querySelector('.temperature-description p');
+//console.log(tempDes);
+var iconRefresh=document.querySelector('.refresh-icon');
+//console.log(iconRefresh);
+
+
+
+
+
+// let xhr = new XMLHttpRequest();
+// console.log(xhr);
+// let apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Cairo,eg&appid=80ad91f9c3a151e6a2ff2fde9f9f2950"
+// xhr.open('GET' ,apiUrl)
+
+// xhr.onload= function(){
+//     if(this.status===200){
+//         console.log(this.responseText)
+//         let objects = JSON.parse(this.responseText);
+//         console.log(objects)
+//     }
+// }
+
+// xhr.send()
+
+
+
